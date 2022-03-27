814b0ebbc2d50ba591ac672119c755c688f7bd05
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-27 16:04:54 +0600

    Add files via upload

---
+++ Comment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//Single line comment
+	cout<<"Single line comment"<<endl;
+	/*
+	Multiple
+	line
+	comment
+	*/
+	cout<<"Multiple line comment"<<endl;
+	return 0;
+}
