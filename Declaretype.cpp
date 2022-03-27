f442f17ce530e61eab9312bea12bed67cfac7f53
LeoCode27 <101262535+LeoCode27@users.noreply.github.com>
2022-03-27 17:41:55 +0600

    Create Declaretype.cpp

---
+++ Declaretype.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x;
+	x=9;
+	decltype(x) y;//y data type = x data type
+	y=2727;
+	cout<< "x is: " <<x<<endl;
+	cout<< "y is: " <<y<<endl;
+	return 0;}
